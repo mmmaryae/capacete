e258c8c71d12fc209025d6cbc70f876ada75665e
Alini Silva <alini.silva2005@gmail.com>
2026-03-29 23:19:27 -0300

    Arquivo main

---
+++ main.py
@@ -0,0 +1,5 @@
+from CamCore import app
+
+#garante segurança e ganrante que so rode se esse arquivo for executado
+if __name__ == "__main__":
+    app.run(debug=True) #qualquer mudança feita já vai ser implantada
